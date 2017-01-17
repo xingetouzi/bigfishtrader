1e40d2db85bf2f1441f36053b19f8624be73e747
caimeng <862786917@qq.com>
2017-01-17 10:12:13 +0800

    .

--- bigfishtrader/quotation/oanda_quotation.py
+++ bigfishtrader/quotation/oanda_quotation.py
@@ -1,29 +1,42 @@
-import oandapy
-import pandas as pd
 from bigfishtrader.quotation.base import AbstractPriceHandler
-from bigfishtrader.event import ExitEvent, BarEvent
+from bigfishtrader.event import ExitEvent, BarEvent, TickEvent
+from datetime import datetime
+from threading import Thread
+import pandas as pd
+import oandapy
+import json
 
 
 class OandaStream(oandapy.Streamer):
-    def __init__(self, event_queue, enviornment='practise', access_token=None):
-        super(OandaStream, self).__init__(enviornment, access_token)
+    def __init__(self, event_queue, account_info=None):
+        super(OandaStream, self).__init__(account_info['environment'], account_info['access_token'])
+        self.info = account_info
         self.event_queue = event_queue
 
     def on_success(self, data):
-        pass
+        if 'tick' in data:
+            ticker = data['tick']
+            self.event_queue.put(
+                TickEvent(
+                    ticker['instrument'],
+                    datetime.strptime(ticker['time'], '%Y-%m-%dT%H:%M:%S.%fZ'),
+                    ticker['ask'], ticker['bid']
+                )
+            )
 
     def on_error(self, data):
-        pass
+        print data
 
 
 class OandaQuotation(AbstractPriceHandler):
     def __init__(
             self, event_queue, oanda_stream,
-            mongo_client, trade_type='paper'
+            mongo_client, trade_type='practice'
     ):
         super(OandaQuotation, self).__init__()
         self.event_queue = event_queue
         self.streamer = oanda_stream
+        self.live_streams = {}
         self.client = mongo_client
         self.type = trade_type
 
@@ -40,7 +53,7 @@ class OandaQuotation(AbstractPriceHandler):
 
         self.filter = {'datetime': dt_filter} if len(dt_filter) else {}
 
-        if self.type == 'paper':
+        if self.type == 'practice':
             self.cursor = self.collection.find(
                 self.filter,
                 projection=['datetime', 'closeMid', 'highMid', 'lowMid', 'openMid', 'volume']
@@ -52,6 +65,25 @@ class OandaQuotation(AbstractPriceHandler):
             )
             self._instance.pop('_id')
             self.cursor = self._instance.iterrows()
+            return self._instance
+        elif self.type == 'live':
+            return pd.DataFrame()
+
+    def run(self):
+        if self.type == 'practice':
+            super(OandaQuotation, self).run()
+        elif self.type == 'live':
+            thread = Thread(
+                target=self.streamer.rates, name=self.ticker,
+                kwargs={
+                    'account_id': self.streamer.info['account_id'],
+                    'instruments': [self.ticker]
+                }
+            )
+            thread.start()
+            self.live_streams[self.ticker] = thread
+
+
 
     def next_stream(self):
         while self._is_running:
@@ -71,6 +103,11 @@ class OandaQuotation(AbstractPriceHandler):
         self.event_queue.put(event)
 
 
+
+if __name__ == '__main__':
+    account_info = json.load(open('D:/bigfishtrader/live_account.json'))
+    stream = OandaStream(None, account_info)
+    stream.rates(account_info['account_id'], ['EUR_USD'])
 
 
 
--- bigfishtrader/router/oanda_exchange.py
+++ bigfishtrader/router/oanda_exchange.py
@@ -1,13 +1,18 @@
 import oandapy
 import json
-from bigfishtrader.quotation.base import AbstractPriceHandler
+from bigfishtrader.event import EVENTS
 from bigfishtrader.router.base import AbstractRouter
+from bigfishtrader.engine.handler import Handler
 
 
 class OandaExchange(AbstractRouter):
-    def __init__(self, oanda_api, event_queue, **ticker_info):
+    def __init__(self, oanda_api, event_queue, trade_type='paper', **ticker_info):
         super(OandaExchange, self).__init__()
         self.api = oanda_api
+        self._handlers = {
+            "on_bar": Handler(self.on_bar, EVENTS.BAR, topic="", priority=100),
+            "on_order": Handler(self.on_order, EVENTS.ORDER, topic=".", priority=0)
+        }
 
     def on_cancel(self, event, kwargs=None):
         pass
@@ -24,8 +29,9 @@ class OandaExchange(AbstractRouter):
 
 
 if __name__ == '__main__':
-    account_info = json.load(open('oanda_account.json'))
-    api = oandapy.API(access_token=account_info['access_token'])
+    import requests
+    account_info = json.load(open('D:/bigfishtrader/oanda_account.json'))
+    api = oandapy.API(access_token=str(account_info['access_token']))
     # response = api.create_order(
     #     account_info['login'],
     #     instrument="EUR_USD",
@@ -33,6 +39,8 @@ if __name__ == '__main__':
     #     side='buy',
     #     type='market',
     # )
+
+
 
     response = api.get_trades(account_info['login'])
     print(response)
