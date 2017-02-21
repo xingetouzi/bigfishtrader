015df2b319aafc91b7e0d4b3b47e0eeb6c21000a
BurdenBear <541795600@qq.com>
2017-02-21 18:00:46 +0800

    Merge remote-tracking branch 'remotes/origin/master' into oanda_api
    # Conflicts:
    #	bigfishtrader/event.py
    #	bigfishtrader/portfolio/handlers.py
    #	bigfishtrader/portfolio/position.py
    #	bigfishtrader/router/oanda_exchange.py
    #	examples/oanda_strategy.py
    #	examples/oanda_trade.py

--- examples/back_test.py
+++ examples/back_test.py
@@ -2,7 +2,6 @@ from bigfishtrader.backtest import stock_backtest as back_test
 import pandas as pd
 from examples import stock_strategy
 
-
 if __name__ == '__main__':
     portfolio = back_test.back_test(strategy=stock_strategy)
     print(
