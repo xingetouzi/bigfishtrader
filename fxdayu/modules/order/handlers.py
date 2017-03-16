b6b8365dd522264b6ad90381d8f625a01ec69a55
BurdenBear <541795600@qq.com>
2017-03-16 17:29:57 +0800

    some file forget to

---
+++ fxdayu/modules/order/handlers.py
@@ -0,0 +1,373 @@
+# encoding: utf-8
+
+import copy
+
+import pandas as pd
+
+from fxdayu.const import ORDERSTATUS
+from fxdayu.context import ContextMixin
+from fxdayu.engine.handler import HandlerCompose, Handler
+from fxdayu.event import EVENTS, OrderEvent
+from fxdayu.models.data import OrderStatusData, Security, OrderReq
+from fxdayu.modules.order.style import *
+from fxdayu.utils.api_support import api_method
+
+
+class AbstractOrderHandler(HandlerCompose, ContextMixin):
+    def __init__(self, engine, context, environment, data):
+        """
+
+        Args:
+            engine(fxdayu.engine.Engine): event engine
+            context:
+            environment(fxdayu.environment.Environment):
+
+        Returns:
+
+        """
+        super(AbstractOrderHandler, self).__init__(engine)
+        ContextMixin.__init__(self, context, environment, data)
+        #  Order.clOrderID should be confirmed before this handler be called in working stream
+        self._handlers["on_order"] = Handler(self.on_order, EVENTS.ORDER, topic=".", priority=-100)
+        self._handlers["on_execution"] = Handler(self.on_execution, EVENTS.EXECUTION, topic=".", priority=-100)
+        self._handlers["on_order_status"] = Handler(self.on_order_status, EVENTS.ORD_STATUS, topic=".", priority=-100)
+
+    def on_order(self, order, kwargs=None):
+        raise NotImplementedError
+
+    def on_order_status(self, status, kwargs=None):
+        raise NotImplementedError
+
+    def on_execution(self, execution, kwargs=None):
+        raise NotImplementedError
+
+    def link_context(self):
+        raise NotImplementedError
+
+
+class OrderStatusHandler(AbstractOrderHandler):
+    def __init__(self, engine, context, environment, data):
+        super(OrderStatusHandler, self).__init__(engine, context, environment, data)
+        self._adapter = OrderReqAdapter(context, environment)
+        self._orders = {}
+        self._open_orders = {}
+        self._executions = {}
+        self._order_status = {}
+
+    def on_order(self, event, kwargs=None):
+        """
+        Args:
+            event(fxdayu.event.OrderEvent): order event
+            kwargs(dict): other optional parameters
+
+        Returns:
+
+        """
+        order = event.data
+        if order.clOrdID:
+            self._orders[order.gClOrdID] = order
+            if order.gSymbol not in self._open_orders:
+                self._open_orders[order.gSymbol] = []
+            self._open_orders[order.gSymbol].append(order)
+            status = OrderStatusData()
+            status.exchange = order.exchange
+            status.symbol = order.symbol
+            status.clOrdID = order.clOrdID
+            status.action = order.action
+            status.side = order.side
+            status.price = order.price
+            status.orderQty = order.orderQty
+            status.leavesQty = order.orderQty
+            status.ordStatus = ORDERSTATUS.GENERATE.value
+            status.gateway = order.gateway
+            status.account = order.account
+            self._order_status[status.gClOrdID] = status
+        else:
+            pass  # TODO warning Order send failed
+
+    def on_execution(self, event, kwargs=None):
+        """
+
+        Args:
+            event(fxdayu.event.ExecutionEvent):
+            kwargs:
+
+        Returns:
+
+        """
+        execution = event.data
+        self._executions[execution.execID] = execution
+
+    def on_order_status(self, event, kwargs=None):
+        """
+
+        Args:
+            event(fxdayu.event.OrderStatusEvent):
+            kwargs:
+
+        Returns:
+            None
+        """
+        status_new = event.data
+        status_old = self._order_status[status_new.gClOrdID]
+        status_old.ordStatus = status_new.ordStatus
+        status_old.cumQty = status_new.cumQty
+        status_old.leavesQty = status_new.leavesQty
+        status_old.orderTime = status_new.orderTime
+        status_old.cancelTime = status_new.cancelTime
+        if status_new.ordStatus == ORDERSTATUS.ALLTRADED.value or status_new.ordStatus == ORDERSTATUS.CANCELLED.value:
+            order = self._orders[status_new.gClOrdID]
+            try:
+                self._open_orders[order.gSymbol].remove(order)
+            except ValueError:
+                pass
+
+    def get_order_status(self, order):
+        """
+
+        Args:
+            order:
+
+        Returns:
+            fxdayu.models.data.OrderStatusData
+        """
+        return self._order_status.get(order, None)
+
+    @api_method
+    def get_order(self, order):
+        return copy.copy(self._orders[order])
+
+    @api_method
+    def get_open_orders(self, security):
+        if isinstance(security, Security):
+            security = security.sid
+        if security is None:
+            return copy.deepcopy(self._open_orders)
+        elif security in self._open_orders:
+            return copy.deepcopy(self._open_orders[security])
+        else:
+            return {}
+
+    def _miss_security(self):
+        pass  # TODO warning
+
+    def get_executions(self, method="df"):
+        if method == "df":
+            df = pd.DataFrame(list(map(lambda x: x.to_dict(ordered=True), self._executions.values())))
+            if not df.empty:
+                df = df.set_index("time", drop=True).sort_index()
+            return df
+        elif method == "list":
+            return copy.deepcopy(self._executions)
+
+    @api_method
+    def order(self, security, amount, limit_price=None, stop_price=None, style=None):
+        """
+        发送所指定手数amount的给定证券security的订单。从所使用的style参数推断订单类型。
+        如果仅传入security和amount参数，则将订单视为为市价订单。
+
+        Args:
+            security(str | fxdayu.models.data.Security): 证券，可以是证券代码或者Security对象。
+            amount(int): 交易手数，整数。正值意味着买入，负值意味着卖出。
+            style(fxdayu.modules.order.style.OrderType): (可选)指定订单样式，默认值为市价订单。可用的订单样式有：
+                style = MarketOrder(exchange)
+                style = StopOrder(stop_price, exchange)
+                style = LimitOrder(limit_price, exchange)
+                style = StopLimitOrder(limit_price=price1, stop_price=price2, exchange)
+
+        Returns:
+            fxdayu.models.data.OrderReq: 订单对象。
+        """
+        if not style:
+            if limit_price and stop_price:
+                style = StopLimitOrder(limit_price, stop_price)
+            elif limit_price:
+                style = LimitOrder(limit_price)
+            elif stop_price:
+                style = StopOrder(stop_price)
+            else:
+                style = MarketOrder()
+        order = self._adapter.parse(security, amount, style)
+        if order:
+            event = OrderEvent(order)
+            self.engine.put(event)
+            return order
+        else:
+            self._miss_security()
+
+    @api_method
+    def order_target(self, security, amount, style=None):
+        """
+
+        Args:
+            security(str | fxdayu.models.data.Security): 证券，可以是证券代码或者Security对象。
+            amount(int): 目标手数，整数。正值意味多头头寸，负值意味着空头头寸。（股票中若传入负值将报错）
+            style(fxdayu.modules.order.style.OrderType): (可选)指定订单样式，默认值为市价订单。可用的订单样式有：
+                style = MarketOrder(exchange)
+                style = StopOrder(stop_price, exchange)
+                style = LimitOrder(limit_price, exchange)
+                style = StopLimitOrder(limit_price=price1, stop_price=price2, exchange)
+
+        Returns:
+            fxdayu.models.data.OrderReq: 订单对象。
+        """
+        if not isinstance(security, Security):
+            security = self.environment.symbol(security)
+        amount = int(amount)
+        if security:
+            position = self.context.portfolio.positions.get(security.sid)
+            if position:
+                volume = position.volume
+            else:
+                volume = 0
+            delta = amount - volume
+            if delta != 0:
+                return self.order(security, delta, style=style)
+            else:
+                return None
+        else:
+            self._miss_security()
+
+    def _value2shares(self, security, value):
+        # TODO 完成实时的current和history遍写
+        point = self.data.current(security.symbol, ["close"])
+        point_value = security.point_value if hasattr(security, "point_value") else 1
+        return int(value / point / point_value)
+
+    def _percent2shares(self, security, percent):
+        value = self.context.portfolio.portfolio_value * percent
+        return self._value2shares(security, value)
+
+    @api_method
+    def order_value(self, security, value, style=None):
+        """
+        根据给定价值value而不是给定的交易手数下单。传入负值代表卖出。交易手数总是被截断为整数手。
+
+        Args:
+            security(str | fxdayu.models.data.Security): 证券，可以是证券代码或者Security对象。
+            value(float): 证券的价值，据此计算交易手数，并截断为整数手。正值意味着买入，负值意味着卖出。
+            style(fxdayu.modules.order.style.OrderType): (可选)指定订单样式，默认值为市价订单。可用的订单样式有：
+                style = MarketOrder(exchange)
+                style = StopOrder(stop_price, exchange)
+                style = LimitOrder(limit_price, exchange)
+                style = StopLimitOrder(limit_price=price1, stop_price=price2, exchange)
+        Examples:
+            发送价值达￥10000的股票代码000002所代表股票的订单：order_value(symbol('000002'),10000)
+            如果000002的价格是每股15元，这将购买6手(600股)，小数部分手数将被截断（不考虑滑点和交易成本）。
+
+        Returns:
+            fxdayu.models.data.OrderReq: 订单对象。
+        """
+
+        if not isinstance(security, Security):
+            security = self.environment.symbol(security)
+        if security:
+            return self.order(security, self._value2shares(security, value), style=style)
+        else:
+            self._miss_security()
+
+    @api_method
+    def order_target_value(self, security, value, style=None):
+        """
+        根据目标头寸价值value计算目标头寸手数，并截取整数手。正值意味多头头寸，负值意味着空头头寸。
+        然后按order_target中的方式下达订单。
+
+        Args:
+            security(str | fxdayu.models.data.Security): 证券，可以是证券代码或者Security对象。
+            value(float): 目标头寸价值，据此计算目标头寸手数，并截断为整数手。正值意味多头头寸，负值意味着空头头寸。
+            style(fxdayu.modules.order.style.OrderType): (可选)指定订单样式，默认值为市价订单。可用的订单样式有：
+                style = MarketOrder(exchange)
+                style = StopOrder(stop_price, exchange)
+                style = LimitOrder(limit_price, exchange)
+                style = StopLimitOrder(limit_price=price1, stop_price=price2, exchange)
+
+        Returns:
+            fxdayu.models.data.OrderReq: 订单对象。
+        """
+        if not isinstance(security, Security):
+            security = self.environment.symbol(security)
+        if security:
+            return self.order_target(security, self._value2shares(security, value), style=style)
+        else:
+            self._miss_security()
+
+    @api_method
+    def order_percent(self, security, percent, style=None):
+        """
+        发送对应于当前资产净值的给定百分比（即头寸总市值和期末现金余额的总和）的订单。传入负百分比值表示卖出。订单总是被截断为全股。百分比必须以小数表示（0.50表示50％）。
+
+        Args:
+            security(str | fxdayu.models.data.Security): 证券，可以是证券代码或者Security对象。
+            percent(float): 百分比。正值意味着买入，负值意味着卖出。
+            style(fxdayu.modules.order.style.OrderType): (可选)指定订单样式，默认值为市价订单。可用的订单样式有：
+                style = MarketOrder(exchange)
+                style = StopOrder(stop_price, exchange)
+                style = LimitOrder(limit_price, exchange)
+                style = StopLimitOrder(limit_price=price1, stop_price=price2, exchange)
+
+        Examples:
+            order_percent(symbol('000002'),.5)将买入价值当前投资组合50%的股票000002。
+            如果000002是15元/股，投资组合价值是100000元，这将购买33手（不考虑滑点和交易成本）。
+
+        Returns:
+            fxdayu.models.data.OrderReq: 订单对象。
+        """
+        if not isinstance(security, Security):
+            security = self.environment.symbol(security)
+        if security:
+            return self.order(security, self._percent2shares(security, percent), style=style)
+        else:
+            self._miss_security()
+
+    @api_method
+    def order_target_percent(self, security, percent, style=None):
+        """
+        根据目标头寸占当前账户净值百分比数计算目标头寸手数，并截取整数手。正值意味多头头寸，负值意味着空头头寸。
+        然后按order_target中的方式下达订单。
+
+        Args:
+            security(str | fxdayu.models.data.Security): 证券，可以是证券代码或者Security对象。
+            percent(float): 目标头寸价值占当前账户净值百分比数。正值意味多头头寸，负值意味着空头头寸。
+            style(fxdayu.modules.order.style.OrderType): (可选)指定订单样式，默认值为市价订单。可用的订单样式有：
+                style = MarketOrder(exchange)
+                style = StopOrder(stop_price, exchange)
+                style = LimitOrder(limit_price, exchange)
+                style = StopLimitOrder(limit_price=price1, stop_price=price2, exchange)
+
+        Returns:
+            fxdayu.models.data.OrderReq: 订单对象。
+        """
+        if not isinstance(security, Security):
+            security = self.environment.symbol(security)
+        if security:
+            return self.order_target(security, self._percent2shares(security, percent), style=style)
+        else:
+            self._miss_security()
+
+    @api_method
+    def cancel_order(self, order):
+        if isinstance(order, OrderReq):
+            order = order.clOrdID
+        else:
+            return
+
+    def link_context(self):
+        # find order
+        self.environment["get_order"] = self.get_order
+        self.environment["get_open_orders"] = self.get_open_orders
+
+        # place order
+        self.environment["order"] = self.order
+        self.environment["order_target"] = self.order_target
+        self.environment["order_value"] = self.order_value
+        self.environment["order_target_value"] = self.order_target_value
+        self.environment["order_percent"] = self.order_percent
+        self.environment["order_target_percent"] = self.order_target_percent
+        self.environment["get_order_status"] = self.get_order_status
+        self.environment["cancel_order"] = self.cancel_order
+
+        # style
+        self.environment["MarketOrder"] = MarketOrder
+        self.environment["StopOrder"] = MarketOrder
+        self.environment["LimitOrder"] = LimitOrder
+        self.environment["StopLimitOrder"] = StopLimitOrder
---
+++ fxdayu/modules/portfolio/handlers.py
@@ -0,0 +1,334 @@
+# encoding: utf-8
+
+import copy
+import time
+
+from dictproxyhack import dictproxy
+import numpy as np
+from enum import Enum
+
+from fxdayu.const import *
+from fxdayu.engine.handler import HandlerCompose, Handler
+from fxdayu.event import EVENTS
+from fxdayu.models.data import PositionData
+from fxdayu.context import ContextMixin, InitializeMixin
+
+STAGE = {
+    ORDERSTATUS.UNKNOWN.value: 0,
+    ORDERSTATUS.GENERATE.value: 1,
+    ORDERSTATUS.NOTTRADED.value: 2,
+    ORDERSTATUS.ALLTRADED.value: 3,
+    ORDERSTATUS.CANCELLED.value: 4,
+}
+
+
+class PortfolioHandler(HandlerCompose, ContextMixin, InitializeMixin):
+    class MODE(Enum):
+        BROKER = 0  # 以按一定频率从交易所获取的仓位数据为准
+        SYNC = 1  # 以本地仓位为准，异步更新(收到报单回报才更新)
+        STRATEGY = 2  # 以本地仓位为准，同步更新(发单即更新)
+
+    class SYNCPOLICY(Enum):
+        BROKER = 0
+        STRATEGY = 1
+        NONE = 2
+
+    class EXECUTION_MODE(Enum):
+        AVG = "AVG"  # 持仓均价成交模式,适合股票
+        FIFO = "FIFO"  # 先进先出成交模式，适合期货
+
+    SYNC_FREQUENCY = 1000  # in millisecond
+    DEFAULT_CAPITAL_BASE = 100000
+
+    def __init__(self, engine, context, environment, data, mode=None, sync_policy=None, execution_mode=None,
+                 has_frozen=False):
+        super(PortfolioHandler, self).__init__(engine)
+        ContextMixin.__init__(self, context, environment, data, use_proxy=True)
+        InitializeMixin.__init__(self)
+        # setting
+        if mode is None:
+            mode = self.MODE.SYNC.value  # 默认同步的方式
+        if sync_policy is None:
+            sync_policy = self.SYNCPOLICY.BROKER.value
+        if execution_mode is None:
+            execution_mode = self.EXECUTION_MODE.AVG.value
+        self._mode = mode
+        # sync policy when there is difference between broker position and strategy position
+        self._sync_policy = sync_policy  # TODO realize the sync logic
+        self._execution_mode = execution_mode
+        self._has_frozen = has_frozen  # Is there frozen volume field in position message from broker
+        self._handlers = {
+            "on_position": Handler(self.on_position, EVENTS.POSITION, topic=".", priority=0),
+            # "on_init_start": Handler(self.on_init_start, EVENTS.INIT, priority=100),
+            "on_init_finish": Handler(self.on_init_finish, EVENTS.INIT, priority=-100)
+        }
+        if self._mode == self.MODE.STRATEGY.value:
+            self._handlers["on_order"] = Handler(self.on_order, EVENTS.ORDER)
+        else:
+            self._handlers["on_order_status"] = Handler(self.on_order_status, EVENTS.ORD_STATUS, topic="", priority=100)
+            self._handlers["on_execution"] = Handler(self.on_execution, EVENTS.EXECUTION)
+        self._handlers['on_time'] = Handler(self.on_time, EVENTS.TIME, topic="bar.close", priority=150)
+
+        # position data
+        self._strategy_positions = {}
+        self._broker_positions = {}
+
+        # back_test
+        self._history_positions = []
+        self._info = []
+
+        self._capital_used = EMPTY_FLOAT
+        self._positions_value = EMPTY_FLOAT
+        self._starting_cash = self.DEFAULT_CAPITAL_BASE
+        self._cash = self.DEFAULT_CAPITAL_BASE
+        self._margin = EMPTY_FLOAT  # TODO realize it
+
+    @staticmethod
+    def get_sign(n):
+        if n > 0:
+            return 1
+        elif n < 0:
+            return -1
+        else:
+            return 0  # position = 0
+
+    @staticmethod
+    def _trans_position(position):
+        """
+
+        Args:
+            position(fxdayu.models.data.PositionData):
+
+        Returns:
+            None
+        """
+        if position.side == DIRECTION.SHORT.value:
+            position.volume = - position.volume
+            position.frozenVolume = - position.frozenVolume
+        position.side = DIRECTION.NET.value
+
+    @staticmethod
+    def _empty_position(sid, data):
+        """
+
+        Args:
+            sid:
+            data(fxdayu.models.data.OrderStatusData | fxdayu.models.data.ExecutionData):
+
+        Returns:
+
+        """
+        position = PositionData()
+        position.account = data.account
+        position.symbol = data.symbol
+        position.sid = sid
+        position.side = DIRECTION.NET.value  # 保存下来的position都使用net模式
+        position.exchange = data.exchange
+        position.gateway = data.gateway
+        position.volume = 0
+        position.frozenVolume = 0
+        position.avgPrice = 0  # avgPrice 暂时不可用
+        return position
+
+    def _close_position(self, position):
+        """
+
+        Args:
+            position(fxdayu.models.data.PositionData):
+
+        Returns:
+
+        """
+        security = self.environment.symbol(position.sid)
+        close = self.data.current(security.symbol).close
+        traded_qty = position.volume - position.frozenVolume
+        if close != np.nan:
+            return traded_qty * (close - position.avgPrice)
+        else:
+            return 0
+
+    def on_init_start(self, event, kwargs=None):
+        self.environment.gateway.qryPosition()
+        # TODO query all open data
+
+    def on_init_finish(self, event, kwargs=None):
+        if self._sync_policy == self.SYNCPOLICY.BROKER.value:
+            self._strategy_positions = copy.deepcopy(self._broker_positions)
+            print(self._strategy_positions)
+
+    def on_position(self, event, kwargs=None):
+        """
+
+        Args:
+            event(fxdayu.event.PositionEvent):
+            kwargs:
+
+        Returns:
+            None
+        """
+        position = event.data
+        security = self.environment.symbol(position.symbol)
+        if security is None:
+            return
+        position.sid = security.sid
+        print("Position: %s" % position.to_dict(ordered=True))
+
+        old = self._broker_positions.pop(position.sid, None)
+        # IB接口中并不给出frozenVolume,只能从openOrder得出
+        self._trans_position(position)
+        if not self._has_frozen and old is not None:
+            position.frozenVolume = old.frozenVolume
+            position.volume += old.frozenVolume
+        if position.volume != 0 or position.frozenVolume != 0:
+            self._broker_positions[position.sid] = position
+
+    def on_order(self, event, kwargs=None):
+        """
+
+        Args:
+            event(fxdayu.event.OrderEvent):
+            kwargs:
+
+        Returns:
+            None
+        """
+        order = event.data
+        # TODO 发单即仓位更新模式
+
+    def on_order_status(self, event, kwargs=None):
+        """
+
+        Args:
+            event(fxdayu.event.OrderStatusEvent):
+            kwargs:
+
+        Returns:
+
+        """
+        new = event.data
+        old = self.environment.get_order_status(new.gClOrdID)
+        # print("old: %s" % new.to_dict(ordered=True))
+        # print("new: %s" % new.to_dict(ordered=True))
+        security = self.environment.symbol(new.symbol)
+        if security is None:
+            return
+        sid = security.sid
+        if STAGE[old.ordStatus] > STAGE[new.ordStatus]:  # error stage of order
+            return  # TODO test order status == unknown
+        sign = 1 if new.side == DIRECTION.LONG.value else -1
+        if old.ordStatus == ORDERSTATUS.GENERATE.value:  # new order execution in first time
+            if sid not in self._strategy_positions:  # create new position
+                self._strategy_positions[sid] = self._empty_position(sid, new)
+            position = self._strategy_positions[sid]
+            position.volume += new.orderQty * sign
+            position.frozenVolume += new.leavesQty * sign
+
+        position = self._strategy_positions[sid]
+        if not self._has_frozen and self._mode == self.MODE.BROKER.value:
+            if sid not in self._broker_positions:
+                if position.frozenVolume:
+                    temp = copy.copy(position)
+                    # there is no realized volume in broker side
+                    temp.volume = temp.frozenVolume
+                    temp.avgPrice = 0
+                    self._broker_positions[sid] = temp
+            else:
+                temp = self._broker_positions[sid]
+                temp.volume -= temp.frozenVolume
+                temp.frozenVolume = position.frozenVolume
+                temp.volume += temp.frozenVolume
+
+    def on_execution(self, event, kwargs=None):
+        """
+
+        Args:
+            event(fxdayu.event.ExecutionEvent):
+            kwargs:
+
+        Returns:
+            None
+        """
+        execution = event.data
+        order = self.environment.get_order_status(execution.gClOrdID)
+        sid = self.environment.symbol(execution.symbol).sid
+        if sid not in self._strategy_positions:
+            self._strategy_positions[sid] = self._empty_position(sid, execution)
+        position = self._strategy_positions[sid]
+        sign_o = 1 if order.side == DIRECTION.LONG.value else -1
+        if order.ordStatus == ORDERSTATUS.GENERATE.value:
+            # # new order execution in first time, and execution arrive before order status
+            position.volume += order.orderQty * sign_o
+            position.frozenVolume += order.leavesQty * sign_o
+            order.ordStatus = ORDERSTATUS.NOTTRADED.value
+        if self._execution_mode == self.EXECUTION_MODE.AVG.value:
+            traded_qty = position.volume - position.frozenVolume
+            last_qty = execution.lastQty * (1 if execution.side == DIRECTION.LONG.value else -1)
+
+            if traded_qty * last_qty >= 0:  # 加仓
+                position.avgPrice = (position.avgPrice * traded_qty + execution.lastPx * last_qty) / \
+                                    (traded_qty + last_qty)
+                self._cash -= last_qty * execution.lastPx
+            else:
+                if abs(traded_qty) < abs(last_qty):  # 反向
+                    position.avgPrice = execution.lastPx if last_qty != traded_qty else 0
+                    self._cash -= last_qty * abs(last_qty - traded_qty)  # 减掉新开仓现金
+                    # 其他情况 avxPrice 不变
+                delta = execution.lastPx if traded_qty > 0 else - execution.lastPx
+                self._cash += delta * min(abs(last_qty), abs(traded_qty))
+            position.frozenVolume -= last_qty
+            if position.volume == 0 and position.frozenVolume == 0:
+                self._strategy_positions.pop(sid)
+        elif self._execution_mode == self.EXECUTION_MODE.FIFO.value:
+            # TODO 先开先平的结算
+            pass
+
+    def on_time(self, event, kwargs=None):
+        for sid, position in self._strategy_positions.items():
+            show = position.to_dict()
+            show["datetime"] = event.time
+            self._history_positions.append(show)
+        self._info.append(
+            {'datetime': event.time, 'cash': self.cash, 'equity': self.portfolio_value},
+        )
+
+    @property
+    def capital_used(self):
+        return self._capital_used
+
+    @property
+    def cash(self):
+        return self._cash
+
+    @property
+    def pnl(self):
+        return self.portfolio_value - self._starting_cash
+
+    @property
+    def portfolio_value(self):
+        return self._cash + sum([self._close_position(p) for p in self.positions.values()]) + self._margin
+
+    @property
+    def positions_value(self):
+        return self._cash + sum([abs(p.volume - p.tradedVolume) * p.avgPrice for p in self.positions.values()])
+
+    @property
+    def returns(self):
+        return self.portfolio_value / self._starting_cash
+
+    @property
+    def positions(self):
+        if self._mode == self.MODE.BROKER.value:
+            return dictproxy(self._broker_positions)
+        else:
+            return dictproxy(self._strategy_positions)
+
+    @property
+    def info(self):
+        return self._info
+
+    @property
+    def history(self):
+        return self._history_positions
+
+    def link_context(self):
+        self.context.portfolio = self
