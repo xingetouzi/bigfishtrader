1b40474c8e8791dabb5348d983a3c1b871171764
caimeng <862786917@qq.com>
2017-03-24 16:43:45 +0800

    SelectorDemo

--- fxdayu/selector/admin.py
+++ fxdayu/selector/admin.py
@@ -1,22 +1,37 @@
 # encoding:utf-8
+from collections import OrderedDict
+
+
+def sort_selector(selectors):
+    dct = OrderedDict()
+
+    for s in sorted(selectors):
+        dct.setdefault(s.rule, []).append(s)
+
+    return dct
 
 
 class SelectorAdmin(object):
-    def __init__(self, context, data):
-        self.context = context
-        self.data = data
-        self.selectors = {}
+    def __init__(self, *selectors):
+        self.selectors = sort_selector(selectors)
 
-    def execution(self):
-        pass
+    def on_time(self, time, context, data):
+        for rule, selectors in self.selectors.items():
+            if rule(time):
+                selectors[0].start(context, data)
+                for selector in selectors:
+                    selector.execute(context, data)
+                selectors[-1].end(context, data)
 
 
 class ExecutorAdmin(object):
-    def __init__(self, context, data, environment):
-        self.context = context
-        self.data = data
-        self.environment = environment
-        self.executors = {}
+    def __init__(self, *executors):
+        self.executors = sort_selector(executors)
 
-    def execution(self):
-        pass
+    def on_time(self, time, context, data, environment):
+        for rule, executors in self.executors.items():
+            if rule(time):
+                executors[0].start(context, data, environment)
+                for executor in executors:
+                    executor.execute(context, data, environment)
+                executors[-1].end(context, data, environment)
